645cfd5af8ea763c2f9ce956258a44dab09b83b8
Bunyod <shbunyod2197@gmail.com>
2026-04-10 16:01:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+# 1-m
+words = ["Hello", "world"]
+result = ",".join(words)
+
+print(result)
+
+
+# 2-m
+items = ["apple", "banana", "cherry"]
+result = ", ".join(items)
+
+print(result)
+
+
+# 3-m
+chars = ["P", "Y", "T", "H", "O", "N"]
+result = "".join(chars)
+
+print(result)
+
+
+# 4-m
+words = ["A", "B", "C", "D"]
+result = "-".join(words)
+
+print(result)
+
+
+# 5-m
+words = ["hello"]
+result = "*".join(words)
+
+print(result)
+
+
+# 6-m
+words = ["Hello", "world"]
+result = "\n".join(words)
+
+print(result)
+
+
+# 7-m
+words = ["backend", "frontend", "devops"]
+result = "|".join(words)
+
+print(result)
+
+
+# 9-m
+words = ["backend", "frontend", "devops"]
+result = "".join(words)
+
+print(result)
+
+
+# 10-m
+words = ["I", "Love", "AI"]
+result = "".join(words)
+
+print(result)
